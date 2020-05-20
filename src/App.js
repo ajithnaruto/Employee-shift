98beda1a7c4f066a10f12df65c6a3f2082fe3346
Ajithkumar P <ajithkumar.p@prodapt.com>
2020-05-20 10:13:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from '../src/components/Home';
+import About from '../src/components/About';
+import Contact from '../src/components/Contact';
+import Error from '../src/components/Error';
+import Navigation from '../src/components/Navigation';
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/about" component={About}/>
+             <Route path="/contact" component={Contact}/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../css/style.css'
+import axios from 'axios';
+class home extends React.Component {
+    constructor() {
+        super();
+        this.showValue = this.showValue.bind(this);
+    }
+
+    showValue(){
+    const user = {
+            support_group: this.support_group.value,
+            day:this.day.value,
+            support_engineer:this.support_engineer.value,
+            support_engineer_phone:this.support_engineer_phone.value,
+            support_engineer_email:this.support_engineer_email.value
+    };
+    console.log(user);
+    let axiosConfig = {
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+        }
+      };
+      
+    axios.post(`http://localhost:8081/create`,user,axiosConfig)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+    }
+
+    render(){
+    return (
+        <div class="row">
+        <div class="col-md-12">
+            <div class="form">
+            <h1> Shift Form </h1>
+              <label for="name">Support Group</label>
+              <input type="text"  name="support_group" ref={el => this.support_group=el}/>
+              <label for="email">Day</label>
+              <input type="text" name="day" ref={el => this.day=el}/>
+                <label for="name">Support Engineer</label>
+              <input type="text"  name="support_engineer" ref={el => this.support_engineer=el}/>
+              <label for="name">Phone</label>
+              <input type="text"  name="support_engineer_phone" ref={el => this.support_engineer_phone=el}/>
+              <label for="email">Email</label>
+              <input type="text" name="support_engineer_email" ref={el => this.support_engineer_email=el}/>
+            <button type="submit" onClick={this.showValue}>Submit</button>
+            </div>
+            </div>
+          </div>
+          
+    );
+}
+}
+export default home;
